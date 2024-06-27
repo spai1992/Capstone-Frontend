9cba724ad5947b58134bf99751f676613df4303c
Simone Giarnera <51135928+spai1992@users.noreply.github.com>
2024-06-27 15:25:37 +0200

    inserimento tasto logout in dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth/authservice.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
