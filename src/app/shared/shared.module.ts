ae4ef3b385e214eeff04d83dfe995f33da2223f6
Simone Giarnera <51135928+spai1992@users.noreply.github.com>
2024-07-07 18:44:23 +0200

    creazione stile landing page

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
-import { NgbModule, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap'; // Importa NgbActiveModal
+import { NgbModule, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { AuthModule } from '../auth/auth.module';
 import { AppointmentModalComponent } from './appointment-modal/appointment-modal.component';
 import { FormsModule } from '@angular/forms';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [NavbarComponent, AppointmentModalComponent],
+  declarations: [NavbarComponent, AppointmentModalComponent, FooterComponent],
   imports: [CommonModule, RouterModule, NgbModule, AuthModule, FormsModule],
-  exports: [NavbarComponent, AppointmentModalComponent],
-  providers: [NgbActiveModal], // Aggiungi NgbActiveModal ai provider
+  exports: [NavbarComponent, AppointmentModalComponent, FooterComponent],
+  providers: [NgbActiveModal],
 })
 export class SharedModule {}
